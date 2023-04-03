149d82c503bc15a2593272b3a39c4fae71628b04
BigBlase <thebigblase@gmail.com>
2023-04-03 11:23:28 +0200

    linted main.py and added a script to pull json / extract zips directly

--- src/main.py
+++ src/main.py
@@ -1,56 +1,54 @@
-import json 
+import json
 
 # WARNING
-# Please download the dataset at 
+# Please download the dataset at
 # https://www.data.gouv.fr/fr/datasets/forets-publiques-diffusion-publique-1/
-# (the first on the page : FOR_PUBL_FR), put in in data_forest
+# (the first on the page : FOR_PUBL_FR), put in in data/data_forest
+# or use the script located in src/pull-data.sh
 
-def get_forest_file () : 
-    f = open("data_forest/FOR_PUBL_FR.json")
-    data = json.load(f)
+def get_forest_file() -> None:
+    """Resume a json file. Outputs a simpler and lighter version of the input file
+    to another file."""
+    with open("data/data_forest/FOR_PUBL_FR.json", encoding="utf-8") as file:
+        data = json.load(file)
 
-    forest_list = []
-    polygon_list = []
+        forest_list = []
+        polygon_list = []
 
-    for i in data["features"]: 
-        forest_list.append([i["properties"]["llib_frt"], i["geometry"]["coordinates"]])
-    
-    with open('data_forest/forests.json', 'w', encoding='utf-8') as json_file:
-        for j in forest_list : 
-            point_cardinaux = [j[1][0][0][0], j[1][0][0][0], j[1][0][0][1], j[1][0][0][1]]
-            for k in j[1][0] : 
-                if k[0] < point_cardinaux[0] :
-                    point_cardinaux[0] = k[0]
-                if k[0] > point_cardinaux[1] :
-                    point_cardinaux[1] = k[0]
-                if k[1] < point_cardinaux[2] :
-                    point_cardinaux[2] = k[1]
-                if k[1] > point_cardinaux[3] :
-                    point_cardinaux[3] = k[1]
+        for i in data["features"]:
+            forest_list.append([i["properties"]["llib_frt"], i["geometry"]["coordinates"]])
 
-            polygon = {"type" : "Feature", 
-                        "geometry" : {
-                            "type" : "Polygon", 
-                            "coordinates" : 
-                            [
+        with open('data/data_forest/forests.json', 'w', encoding='utf-8') as json_file:
+            for j in forest_list:
+                point_cardinaux = [j[1][0][0][0], j[1][0][0][0], j[1][0][0][1], j[1][0][0][1]]
+                for k in j[1][0]:
+                    if k[0] < point_cardinaux[0]:
+                        point_cardinaux[0] = k[0]
+                    if k[0] > point_cardinaux[1]:
+                        point_cardinaux[1] = k[0]
+                    if k[1] < point_cardinaux[2]:
+                        point_cardinaux[2] = k[1]
+                    if k[1] > point_cardinaux[3]:
+                        point_cardinaux[3] = k[1]
+
+                polygon = {"type" : "Feature",
+                            "geometry" : {
+                                "type" : "Polygon",
+                                "coordinates" :
                                 [
-                                    [point_cardinaux[1], point_cardinaux[3]],
-                                    [point_cardinaux[0], point_cardinaux[3]], 
-                                    [point_cardinaux[0], point_cardinaux[2]],
-                                    [point_cardinaux[1], point_cardinaux[2]]
+                                    [
+                                        [point_cardinaux[1], point_cardinaux[3]],
+                                        [point_cardinaux[0], point_cardinaux[3]],
+                                        [point_cardinaux[0], point_cardinaux[2]],
+                                        [point_cardinaux[1], point_cardinaux[2]]
+                                    ]
                                 ]
-                            ]
-                        },
-                        "properties" :  {
-                            "nom" : j[0]
+                            },
+                            "properties" : {
+                                "nom" : j[0]
+                            }
                         }
-                        }
-            polygon_list.append(polygon)
-        json.dump(polygon_list, json_file,ensure_ascii=False)          
-
-    f.close()
-
-    return True
+                polygon_list.append(polygon)
+            json.dump(polygon_list, json_file,ensure_ascii=False)
 
 get_forest_file()
-
--- src/pull-data.sh
+++ src/pull-data.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-wget -O ../data_forest/FOR_PUBL_FR.json https://www.data.gouv.fr/fr/datasets/r/7b0811ee-9c02-435a-a2e8-440f6a4ffca7
+# cd to file location
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+#create and pull data
+mkdir -p ../data/data_cities ../data/data_forest
+
+# make that parallel (faster)
+wget -O ../data/data_cities/output.zip https://www.insee.fr/fr/statistiques/fichier/6683035/ensemble.zip
+wget -O ../data/data_forest/FOR_PUBL_FR.json https://www.data.gouv.fr/fr/datasets/r/7b0811ee-9c02-435a-a2e8-440f6a4ffca7
+
+wait # "join threads" (wait for tasks in bg)
+
+# extract data that needs to be extracted
+cd ../data/data_cities/
+unzip -o ./output.zip
+
+cd $SCRIPT_DIR
