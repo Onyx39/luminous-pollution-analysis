bcd61f16ba11a52066ad4202dd5498a13ce46933
jopruvo <jordan.pruvost11@gmail.com>
2023-04-21 09:59:57 +0200

    tri des villes

---
+++ src/villes.py
@@ -0,0 +1,20 @@
+import json
+
+df = open("ville_json\cities.json")
+data = json.load(df)
+
+
+infos_villes = {"villes":[]}
+for city in data["cities"]:
+    ##print(city["city_code"])
+    ##print(city["latitude"])
+    ##print(city["longitude"])
+    if city["latitude"] != "" and city["latitude"] != "":
+        infos_villes["villes"].append([city["city_code"], city["latitude"], city["longitude"]])
+    ##ville["infos"] = [city["city_code"], city["latitude"], city["longitude"]]
+
+
+json_object = json.dumps(infos_villes, indent = 4) 
+
+with open("ville_json/villes.json", "w") as write_file:
+    json.dump(infos_villes, write_file, indent=4)
