40e5430704b16cef49b4156f56da1a282858faca
Valentin <valentin.richard39@sfr.fr>
2023-04-25 10:34:49 +0200

    reorganiation cities_processing.py + amelioration qualite

--- src/cities_processing.py
+++ src/cities_processing.py
@@ -3,7 +3,10 @@ from tqdm import tqdm
 from shapely.geometry import shape
 
 
-def reduce_shape(geometry, max_points=20):
+def reduce_shape(geometry):
+    """Reduce the number of points of the given shape
+    Returns: a list of coordinates
+    """
     coords = None
     ratio = 0.001
 
@@ -11,68 +14,77 @@ def reduce_shape(geometry, max_points=20):
 
     res = []
 
-    for c in coords.exterior.coords:
-        res.append([c[1], c[0]])
+    for coord in coords.exterior.coords:
+        res.append([coord[1], coord[0]])
     return res
 
 
 def reduce_multi_shape(geometry):
+    """Applies the reduce shape function on a MultiPolygon
+    Returns: a list a reduced shape
+    """
     res = []
     for geom in geometry.geoms:
         res.append(reduce_shape(geom))
     return res
 
+def init_coordonates(geometry):
+    """Initialize the new coordinates and the list of points to visit
+    Returns: The list of points to visit and the initialisation of the new coordonates
+    """
+    coord = geometry["coordinates"]
+    if geometry["type"] == "MultiPolygon":
+        liste_a_parcourir = []
+        for k in coord:
+            liste_a_parcourir += k[0]
+            points_cardinaux = [coord[0][0][0][0], coord[0][0][0][0],
+                               coord[0][0][0][1], coord[0][0][0][1]]
+        return liste_a_parcourir, points_cardinaux
+
+    liste_a_parcourir = coord[0]
+    points_cardinaux = [coord[0][0][0], coord[0][0][0],
+                       coord[0][0][1], coord[0][0][1]]
+    return liste_a_parcourir, points_cardinaux
+
+
 
 def create_cities_file():
+    """Create a json file that contains all needed data about cities
+    Returns: boolean (creates automatically the file)
+    """
     # Link of the data:
     # https://github.com/gregoiredavid/france-geojson/blob/master/communes.geojson
     print("Importing data...")
-    file = open("data/cities/communes.geojson", encoding="utf-8")
-    data = json.load(file)
+    with open("data/cities/communes.geojson", encoding="utf-8") as file:
+        data = json.load(file)
 
-    forest_list = []
+    cities_list = []
 
     for i in data["features"]:
-        forest_list.append(
-                [i["properties"]["nom"],
-                 i["geometry"]["coordinates"],
-                 i["geometry"]["type"],
-                 i["geometry"],
-                 i["properties"]["code"]])
+        cities_list.append([i["properties"], i["geometry"]])
 
     print("Writing file...")
-    with open('data/cities/cities_output_Valentin.json', 'w',
-              encoding='utf-8') as json_file:
+    with open('data/cities/cities_output_Valentin.json', 'w',encoding='utf-8') as json_file:
         json_file.write("[\n")
         compteur = 1
-        p_bar = tqdm(total=len(forest_list))
-        for j in forest_list:
-            if j[2] == "MultiPolygon":
-                liste_a_parcourir = []
-                for k in j[1]:
-                    liste_a_parcourir += k[0]
-                point_cardinaux = [j[1][0][0][0][0], j[1][0][0][0][0],
-                                   j[1][0][0][0][1], j[1][0][0][0][1]]
-
-            else:
-                liste_a_parcourir = j[1][0]
-                point_cardinaux = [j[1][0][0][0], j[1][0][0][0],
-                                   j[1][0][0][1], j[1][0][0][1]]
+        p_bar = tqdm(total=len(cities_list))
+        for [prop, geom] in cities_list:
+            liste_a_parcourir, points_cardinaux = init_coordonates(geom)
 
             for k in liste_a_parcourir:
-                if k[0] < point_cardinaux[0]:
-                    point_cardinaux[0] = k[0]
-                if k[0] > point_cardinaux[1]:
-                    point_cardinaux[1] = k[0]
-                if k[1] < point_cardinaux[2]:
-                    point_cardinaux[2] = k[1]
-                if k[1] > point_cardinaux[3]:
-                    point_cardinaux[3] = k[1]
+                if k[0] < points_cardinaux[0]:
+                    points_cardinaux[0] = k[0]
+                if k[0] > points_cardinaux[1]:
+                    points_cardinaux[1] = k[0]
+                if k[1] < points_cardinaux[2]:
+                    points_cardinaux[2] = k[1]
+                if k[1] > points_cardinaux[3]:
+                    points_cardinaux[3] = k[1]
 
-            if j[4][:2] == "2A" or j[4][:2] == "2B":
+            if prop["code"][:2] == "2A" or prop["code"][:2] == "2B":
                 dep = 20
             else:
-                dep = int(j[4][:2])
+                dep = int(prop["code"][:2])
 
             polygon = {
                 "type": "Feature",
@@ -80,33 +92,32 @@ def create_cities_file():
                             "type": "Polygon",
                             "coordinates": [
                                     [
-                                        [point_cardinaux[1],
-                                         point_cardinaux[3]],
-                                        [point_cardinaux[0],
-                                         point_cardinaux[3]],
-                                        [point_cardinaux[0],
-                                         point_cardinaux[2]],
-                                        [point_cardinaux[1],
-                                         point_cardinaux[2]]
+                                        [points_cardinaux[1],
+                                         points_cardinaux[3]],
+                                        [points_cardinaux[0],
+                                         points_cardinaux[3]],
+                                        [points_cardinaux[0],
+                                         points_cardinaux[2]],
+                                        [points_cardinaux[1],
+                                         points_cardinaux[2]]
                                     ]
                                 ],
                             },
                         "properties": {
-                            "nom": j[0],
+                            "nom": prop["nom"],
                             "dep": dep
                         }
                     }
 
-            if j[2] == 'MultiPolygon':
-                polygon['geometry']['MultiShape'] = [
-                    reduce_multi_shape(shape(j[3]))
-                    ]
+            if geom["type"] == 'MultiPolygon':
+                polygon['geometry']['MultiShape'] = [reduce_multi_shape(shape(geom))]
+
             else:
-                polygon['geometry']['shape'] = [reduce_shape(shape(j[3]))]
+                polygon['geometry']['shape'] = [reduce_shape(shape(geom))]
 
             json.dump(polygon, json_file, ensure_ascii=False)
             compteur += 1
-            if not compteur == len(forest_list) + 1:
+            if compteur != len(cities_list) + 1:
                 json_file.write(",\n")
             p_bar.update(1)
 
